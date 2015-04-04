ec079331e7e4082d20206ac27aa9765af229c234
Lajos Santa <santa.lajos@coldline.hu>
2015-04-04 18:34:14 +0200

    Release 1.0

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name = "plutonium-plugin-configui-web",
     description = "Web based config user interface plugin for Plutonium",
-    version = '0.2',
+    version = '1.0',
     install_requires = [
-        'plutonium',
-        'sqlchemyforms'
+        'plutonium == 1.0',
+        'sqlchemyforms == 1.0',
     ],
     include_package_data = True,
     packages = find_packages(),
